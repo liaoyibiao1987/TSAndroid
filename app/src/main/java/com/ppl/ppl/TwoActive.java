cd021e4c361e05eb7505fddbbfaa8c0fccb20c37
liaoyibiao1987 <zlyun616@gmail.com>
2016-12-13 23:45:03 +0800

    2016-12-13家里

--- app/src/main/java/com/ppl/ppl/TwoActive.java
+++ app/src/main/java/com/ppl/ppl/TwoActive.java
@@ -2,14 +2,32 @@ package com.ppl.ppl;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TwoActive extends AppCompatActivity {
     private TApp app;
+    private ListView contextMenulist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_two_active);
         app = (TApp) getApplication();
+        contextMenulist = (ListView) findViewById(R.id.listContacter);
+
+        ArrayAdapter<String> stringapd = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, GetData());
+        contextMenulist.setAdapter(stringapd);
+    }
+
+    private List<String> GetData() {
+        List<String> list = new ArrayList<String>();
+        for (int i = 0; i < 10; i++) {
+            list.add("name: " + i);
+        }
+        return list;
     }
 }
