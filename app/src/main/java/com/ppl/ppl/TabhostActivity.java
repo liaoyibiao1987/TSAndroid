ac6c7de208d612983f7363be69fe917bcab63115
liaoyibiao1987 <zlyun616@gmail.com>
2016-12-22 00:15:48 +0800

    2016-12-21家里

--- app/src/main/java/com/ppl/ppl/TabhostActivity.java
+++ app/src/main/java/com/ppl/ppl/TabhostActivity.java
@@ -2,7 +2,12 @@ package com.ppl.ppl;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+/**
+ * @author:作者名字
+ * @Description:  这个控件已经过时
+ * @date：2015年3月25日 下午5:09:29
+ *  ...等等
+ */
 public class TabhostActivity extends AppCompatActivity {
 
     @Override
