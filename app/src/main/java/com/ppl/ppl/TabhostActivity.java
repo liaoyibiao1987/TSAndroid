8956c1abc310210197b651f87fdb772ba3397b42
liaoyibiao1987 <zlyun616@gmail.com>
2016-12-21 23:55:42 +0800

    2016-12-21家里

---
+++ app/src/main/java/com/ppl/ppl/TabhostActivity.java
@@ -0,0 +1,13 @@
+package com.ppl.ppl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TabhostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tabhost);
+    }
+}
