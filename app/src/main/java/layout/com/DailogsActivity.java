627385128a994129a7957635aad8323652bbef17
liaoyibiao1987 <zlyun616@gmail.com>
2017-01-03 00:34:29 +0800

    2017-1-2家里

--- app/src/main/java/layout/com/DailogsActivity.java
+++ app/src/main/java/layout/com/DailogsActivity.java
@@ -5,6 +5,8 @@ import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Toast;
 
 import com.ppl.ppl.R;
@@ -25,5 +27,7 @@ public class DailogsActivity extends AppCompatActivity {
         dialog.show();
         Toast toast = Toast.makeText(this, "测试信息", Toast.LENGTH_SHORT);
         toast.setGravity(Gravity.LEFT | Gravity.TOP, 0, 0);
+
+        View view = LayoutInflater.from(DailogsActivity.this).inflate(R.layout.activity_clocker, null);
     }
 }
