fed164845252b0a3570c1c9192493a34e576f474
liaoyibiao1987 <zlyun616@gmail.com>
2017-09-02 18:44:14 +0800

    2017年9月2日公司

--- com.ppl.test1/src/main/java/com/example/comppltest1/MainActivity.java
+++ com.ppl.test1/src/main/java/com/example/comppltest1/MainActivity.java
@@ -10,40 +10,8 @@ public class MainActivity extends AppCompatActivity {
 
     Button btnstop;
     Button btnstart;
-
-    private Subscriber<Contributor> contributorSub = new Subscriber<Contributor>() {
-
-        @Override
-        public void onStart() {
-            showProgress();
-        }
-
-        @Override
-        public void onCompleted() {
-
-        }
-
-        @Override
-        public void onError(Throwable e) {
-
-        }
-
-        @Override
-        public void onNext(Contributor contributor) {
-            binding.setContributor(contributor);
-
-            dismissProgress();
-        }
-    };
-
-    private ProcessDialog dialog;
-
-    private MvvmActivityMainBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this, R.layout.mvvm_activity_main);
 
         requestWindowFeature(Window.FEATURE_PROGRESS);
         //requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
@@ -68,46 +36,7 @@ public class MainActivity extends AppCompatActivity {
                 setProgressBarVisibility(false);
             }
         });
-    }
 
-    public void get(View view){
-        getContributors("square", "retrofit");
-    }
 
-    public void change(View view){
-        if(binding.getContributor() != null){
-            binding.getContributor().setLogin("zjutkz");
-        }
-    }
-
-    public void showProgress(){
-        if(dialog == null){
-            dialog = new ProcessDialog(this);
-        }
-
-        dialog.showMessage("正在加载...");
-    }
-
-    public void dismissProgress(){
-        if(dialog == null){
-            dialog = new ProcessDialog(this);
-        }
-
-        dialog.dismiss();
-    }
-
-    public void getContributors(String owner,String repo){
-        GitHubApi.getContributors(owner, repo)
-                .take(1)
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeOn(Schedulers.newThread())
-                .map(new Func1<List<Contributor>, Contributor>() {
-
-                    @Override
-                    public Contributor call(List<Contributor> contributors) {
-                        return contributors.get(0);
-                    }
-                })
-                .subscribe(contributorSub);
     }
 }
