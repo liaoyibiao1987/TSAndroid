9ab2ebe5a1c1d9159531a609e4829846a4151bf1
liaoyibiao1987 <zlyun616@gmail.com>
2017-09-21 18:39:15 +0800

    2017年9月21日公司

--- crazyandroid/src/main/java/com/dy/crazyandroid/FormsTestActivity.java
+++ crazyandroid/src/main/java/com/dy/crazyandroid/FormsTestActivity.java
@@ -1,16 +1,32 @@
 package com.dy.crazyandroid;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Toast;
 import android.widget.ToggleButton;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
 
 public class FormsTestActivity extends AppCompatActivity {
 
     private int mThemeId = -1; // 皮肤主题ID，默认-1 不处理
     private ToggleButton toggleButton;
+    private Button btn_getnetworkstatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +53,29 @@ public class FormsTestActivity extends AppCompatActivity {
             }
         });
 
+        btn_getnetworkstatus = (Button) findViewById(R.id.btn_getnetworkstatus);
+        btn_getnetworkstatus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String x = getIPAddress(FormsTestActivity.this);
+                new AlertDialog.Builder(FormsTestActivity.this).setTitle("IP提示")//设置对话框标题
+                        .setMessage("获取到的IP信息！" + x)//设置显示的内容
+                        .setPositiveButton("确定", new DialogInterface.OnClickListener() {//添加确定按钮
+                            // @Override
+                            public void onClick(DialogInterface dialog, int which) {//确定按钮的响应事件
+                                // TODO Auto-generated method stub
+
+                            }
+                        }).setNegativeButton("返回", new DialogInterface.OnClickListener() {//添加返回按钮
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {//响应事件
+                        // TODO Auto-generated method stub
+                        Log.i("alertdialog", " 请保存数据！");
+                    }
+                }).show();//在按键响应事件中显示此对话框
+            }
+        });
+
     }
 
     private void onTheme(int iThemeId) {
@@ -49,5 +88,52 @@ public class FormsTestActivity extends AppCompatActivity {
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putInt("theme", mThemeId);
+    }
+
+
+    public static String getIPAddress(Context context) {
+        NetworkInfo info = ((ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE)).getActiveNetworkInfo();
+        if (info != null && info.isConnected()) {
+            if (info.getType() == ConnectivityManager.TYPE_MOBILE) {//当前使用2G/3G/4G网络
+                try {
+                    //Enumeration<NetworkInterface> en=NetworkInterface.getNetworkInterfaces();
+                    for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements(); ) {
+                        NetworkInterface intf = en.nextElement();
+                        for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements(); ) {
+                            InetAddress inetAddress = enumIpAddr.nextElement();
+                            if (inetAddress.isLoopbackAddress() == false && inetAddress instanceof Inet4Address) {
+                                return inetAddress.getHostAddress();
+
+                            }
+                        }
+                    }
+                } catch (SocketException e) {
+                    e.printStackTrace();
+                }
+
+            } else if (info.getType() == ConnectivityManager.TYPE_WIFI) {//当前使用无线网络
+                WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+                WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+                String ipAddress = intIP2StringIP(wifiInfo.getIpAddress());//得到IPV4地址
+                return ipAddress;
+            }
+        } else {
+            //当前无网络连接,请在设置中打开网络
+        }
+        return null;
+    }
+
+    /**
+     * 将得到的int类型的IP转换为String类型
+     *
+     * @param ip
+     * @return
+     */
+    public static String intIP2StringIP(int ip) {
+        return (ip & 0xFF) + "." +
+                ((ip >> 8) & 0xFF) + "." +
+                ((ip >> 16) & 0xFF) + "." +
+                (ip >> 24 & 0xFF);
     }
 }
